bc17804b6140f7a6152800981e64341f450f5111
Milana Maksina <m_maksinaa@mail.ru>
2022-09-13 13:55:42 +0300

    feat: add validation

--- CustomerRepositoryPattern/Entities/Address.cs
+++ CustomerRepositoryPattern/Entities/Address.cs
@@ -1,14 +1,21 @@
-﻿namespace CustomerRepositoryPattern.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerRepositoryPattern.Entities
 {
     public class Address
     {
         public int AddressId { get; set; }
         public int CustomerId { get; set; }
+        [Required(ErrorMessage = "AddressLine is required"), MaxLength(100, ErrorMessage = "Max length should be less than 100")]
         public string AddressLine { get; set; }
+        [MaxLength(100, ErrorMessage = "Length should be less than 100")]
         public string AddressLine2 { get; set; }
         public string AddressType { get; set; }
+        [MaxLength(50, ErrorMessage = "Length should be less than 50")]
         public string City { get; set; }
+        [MaxLength(6, ErrorMessage = "Length should be less than 6")]
         public string PostalCode { get; set; }
+        [MaxLength(20, ErrorMessage = "Length should be less than 20")]
         public string State { get; set; }
         public string Country { get; set; }
     }
--- CustomerRepositoryPattern/Entities/Customer.cs
+++ CustomerRepositoryPattern/Entities/Customer.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CustomerRepositoryPattern.Entities
 {
     public class Customer
     {
         public int CustomerId { get; set; }
+        [MaxLength(50, ErrorMessage = "The length can't be more than 50 characters")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "LastName is required"), MaxLength(50, ErrorMessage = "The length can't be more than 50 characters")]
         public string LastName { get; set; }
+        [MaxLength(15, ErrorMessage = "Invalid Phone")]
         public string PhoneNumber { get; set; }
+        [EmailAddress(ErrorMessage = "Invalid Email")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Notes are required")]
         public string Notes { get; set; }
         public decimal? TotalPurchasesAmount { get; set; }
     }
